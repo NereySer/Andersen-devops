52e1eadb4105a874e03b0635c47a403c0b9ccdcb
NereySer <nerey_ser@tut.by>
2021-12-12 20:07:51 +0300

    Update aws.tf
    Usage of for_each is added

--- lesson 8/aws.tf
+++ lesson 8/aws.tf
@@ -18,32 +18,26 @@ variable "image_id" {
   default = "ami-0c7ea5497c02abcaf" # Debian 10
 }
 
-variable "subnet_A_id" {
-  default = "subnet-028cdc74e61702420"
+variable "subnets_id" {
+  default = ["subnet-028cdc74e61702420", "subnet-036d8d2dcbcadf3d1"]
 }
 
-variable "subnet_B_id" {
-  default = "subnet-036d8d2dcbcadf3d1"
-}
-
-variable "av_zone_A" {
-  default = "us-west-2a"
-}
-
-variable "av_zone_B" {
-  default = "us-west-2b"
+variable "av_zones" {
+  default = ["us-west-2a", "us-west-2b"]
 }
 
 variable "vpc_id" {
   default = "vpc-021ca6cda34354a3a"
 }
 
-resource "aws_instance" "app_A" {
+resource "aws_instance" "applications" {
+  for_each = zipmap(var.av_zones, var.subnets_id)
+
   ami                    = var.image_id
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.allow_app_traffic.id}"]
-  subnet_id              = var.subnet_A_id
-  availability_zone      = var.av_zone_A
+  subnet_id              = each.value
+  availability_zone      = each.key
   user_data              = <<EOF
 #!/bin/bash
 sudo apt update && sudo apt install -y nginx
@@ -57,24 +51,6 @@ sudo systemctl restart nginx
 EOF
 }
 
-resource "aws_instance" "app_B" {
-  ami                    = var.image_id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.allow_app_traffic.id}"]
-  subnet_id              = var.subnet_B_id
-  availability_zone      = var.av_zone_B
-  user_data              = <<EOF
-#!/bin/bash
-sudo apt update && sudo apt install -y nginx
-export AWS_ACCESS_KEY_ID=<...placeholder...>
-export AWS_SECRET_ACCESS_KEY=<...placeholder...>
-export AWS_DEFAULT_REGION=us-west-2
-sudo rm /var/www/html/*
-aws s3 cp s3://mybucket.ru/index.html /tmp/index.html
-sudo mv /tmp/index.html /var/www/html/index.html
-sudo systemctl restart nginx
-EOF
-}
 
 
 
@@ -87,17 +63,14 @@ resource "aws_lb_target_group" "lbtarg" {
   vpc_id   = var.vpc_id
 }
 
-resource "aws_lb_target_group_attachment" "inst_A" {
+resource "aws_lb_target_group_attachment" "instances" {
+  for_each = aws_instance.applications
+
   target_group_arn = aws_lb_target_group.lbtarg.arn
-  target_id        = aws_instance.app_A.id
+  target_id        = each.value.id
   port             = 80
 }
 
-resource "aws_lb_target_group_attachment" "inst_B" {
-  target_group_arn = aws_lb_target_group.lbtarg.arn
-  target_id        = aws_instance.app_B.id
-  port             = 80
-}
 
 
 
@@ -131,7 +104,7 @@ resource "aws_security_group" "allow_app_traffic" {
 resource "aws_lb" "AppLB" {
   internal           = false
   load_balancer_type = "application"
-  subnets            = [var.subnet_A_id, var.subnet_B_id]
+  subnets            = var.subnets_id
 }
 
 resource "aws_lb_listener" "front_end" {
