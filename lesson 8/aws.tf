6a4a007f05f853f1d899e07e16e161c8d2016fcf
NereySer <nerey_ser@tut.by>
2021-12-13 19:47:08 +0300

    Update aws.tf
    Corrected mistake port

--- lesson 8/aws.tf
+++ lesson 8/aws.tf
@@ -87,8 +87,8 @@ resource "aws_security_group" "allow_app_traffic" {
 
   ingress {
     description = "app from anywhere"
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
