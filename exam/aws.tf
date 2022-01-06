5e98d828a2b504b6e00048310989832bfc313101
NereySer <nerey_ser@tut.by>
2022-01-06 17:44:36 +0300

    Update aws.tf
    Added app2 and now performs init not as root

--- exam/aws.tf
+++ exam/aws.tf
@@ -19,6 +19,11 @@ variable "region" {
   default = "us-west-2"
 }
 
+variable "key_pair" {
+  type    = string
+  default = "aws_main"
+}
+
 variable "image_id" {
   type    = string
   default = "ami-0c7ea5497c02abcaf" # Debian 10
@@ -39,32 +44,47 @@ variable "vpc_id" {
   default = "vpc-021ca6cda34354a3a"
 }
 
+output "instance_dns" {
+  value = aws_instance.applications.public_dns
+}
+
 resource "aws_instance" "applications" {
   ami                    = var.image_id
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.allow_app_traffic.id}"]
   subnet_id              = var.subnet_id
   availability_zone      = var.av_zone
-  key_name               = "aws_main"
+  key_name               = var.key_pair
+  tags                   = {}
   user_data              = <<EOF
 #!/bin/bash
 sudo apt update && sudo apt install -y subversion docker.io
+su admin
+
 mkdir ~/app1
 cd ~/app1
 svn cat https://github.com/NereySer/Andersen-devops/trunk/exam/app1/rebuild > ./rebuild
 chmod +x rebuild
 ./rebuild
+
+mkdir ~/app2
+cd ~/app2
+svn cat https://github.com/NereySer/Andersen-devops/trunk/exam/app2/rebuild > ./rebuild
+chmod +x rebuild
+./rebuild
+
 EOF
 }
 
 resource "aws_security_group" "allow_app_traffic" {
   name   = "allow_traffic"
   vpc_id = var.vpc_id
+  tags   = {}
 
   ingress {
     description = "app from anywhere"
     from_port   = 80
-    to_port     = 80
+    to_port     = 81
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
